cd8115d7bf7a8ba58f194e4ec3cfcd41f77a0bff
Yerlan Kussainov <lagox.mail@gmail.com>
2014-05-13 13:19:33 +0600

    edit

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- 
 <?php
 // если мы не знаем реферала
 if (!isset($_COOKIE['referer'])) {
@@ -16,7 +15,6 @@ if (isset($_GET['utm_source']) && !isset($_COOKIE['utm_source'])) {
     }
 }
 ?>
- -->
 <!DOCTYPE html>
 <html lang="ru">
 <head>
